711022549dfb817a598e2414757d4f92cca0321d
Nick Bos <roofingbos@gmail.com>
2022-02-09 20:49:12 +1030

    added pseudo code, score statement, prompt box

--- script.js
+++ script.js
@@ -33,21 +33,67 @@ function  playRound(playerSelection, computerSelection){
         gameMessage = "Paper beats Rock, you win!";
 
     }else if (playerSelection == "scissors" && computerSelection == "rock"){
+        gameMessage = "Rock beats scissors, you lose!";
 
     }else if (playerSelection  == "rock" && computerSelection == "scissors"){
         gameMessage = "Rock beats Scissors, you win!";
 
     }else if (playerSelection == "paper" && computerSelection == "scissors"){
-        gameMessage = "Scissors beats Paper, you lose";
+        gameMessage = "Scissors beats Paper, you lose!";
     }else  return "error"
     
     return gameMessage;
     
 };
 
-const playerSelection = "rock";
+
+
+
+const playerSelection = window.prompt().toLocaleLowerCase()
 const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+playRound(playerSelection, computerSelection)
+
+let playerScore = 0;
+let computerScore = 0;
+
+
+//write game()function that plays 5 rounds
+//write if statement for score counter
+// score must increment and save score each round 
+//consol.log winner after 5 rounds
 
 //  game function
-function game()
+function game(){
+    
+    console.log(playRound(playerSelection, computerSelection))
+
+    if (playerSelection == "rock" && computerSelection == "paper"){
+        computerScore ++;
+    
+
+    }else if (playerSelection == "scissors" && computerSelection == "paper"){
+        playerScore ++;
+
+    }else if (playerSelection == "paper" && computerSelection == "rock"){
+        playerScore ++;
+
+    }else if (playerSelection == "scissors" && computerSelection == "rock"){
+        computerScore ++;
+    }else if (playerSelection  == "rock" && computerSelection == "scissors"){
+        playerScore ++;
+
+    }else if (playerSelection == "paper" && computerSelection == "scissors"){
+        computerScore ++ ; 
+    }
+    console.log(playerScore)
+    console.log(computerScore)
+};
+
+// trouble? find out how to run 1 game at a time in console!
+
+
+
+
+
+game()
+
