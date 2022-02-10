8a2e5cee137fed24b80ca6c910a6c8535b6236de
Nick Bos <roofingbos@gmail.com>
2022-02-10 21:08:33 +1030

    refactoring and updated code

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+debugger
+
 const rock = 0;
 const paper = 1;
 const scissors = 2;
@@ -9,17 +11,48 @@ function getRandomInt(min, max){
 function computerPlay(){
     let value = getRandomInt(0, 2);
       
-if (value === 0){
+    if (value === 0){
     return "rock"
-} else if ( value === 1){
+    } else if ( value === 1){
     return "paper"
-} else return "scissors"
+    } else return "scissors"
 };
 
 
 //play round function
-function  playRound(playerSelection, computerSelection){
+
+
+function  playRound(){
     let gameMessage;
+
+    let computerSelection = computerPlay();
+
+    let playerSelection = prompt("Player Choice").toLocaleLowerCase();
+
+    if (playerSelection == "rock" && computerSelection == "paper"){
+        computerScore ++;
+    
+
+    }else if (playerSelection == "scissors" && computerSelection == "paper"){
+        playerScore ++;
+
+    }else if (playerSelection == "paper" && computerSelection == "rock"){
+        playerScore ++;
+
+    }else if (playerSelection == "scissors" && computerSelection == "rock"){
+        computerScore ++;
+    }else if (playerSelection  == "rock" && computerSelection == "scissors"){
+        playerScore ++;
+
+    }else if (playerSelection == "paper" && computerSelection == "scissors"){
+        computerScore ++ ; 
+    }
+
+    console.log(playerScore)
+    console.log(computerScore)
+
+
+
     if (playerSelection == computerSelection){
         gameMessage = "Draw";
     
@@ -41,59 +74,51 @@ function  playRound(playerSelection, computerSelection){
     }else if (playerSelection == "paper" && computerSelection == "scissors"){
         gameMessage = "Scissors beats Paper, you lose!";
     }else  return "error"
+
+    console.log(gameMessage)
     
-    return gameMessage;
+
+    
     
 };
 
 
 
 
-const playerSelection = window.prompt().toLocaleLowerCase()
-const computerSelection = computerPlay();
-playRound(playerSelection, computerSelection)
+
+ 
+
 
 let playerScore = 0;
 let computerScore = 0;
 
-
 //write game()function that plays 5 rounds
+
+
 //write if statement for score counter
+function scoreLogger(){
+    console.log(playerScore)
+    console.log(computerScore)
+}
+
 // score must increment and save score each round 
 //consol.log winner after 5 rounds
 
 //  game function
 function game(){
     
-    console.log(playRound(playerSelection, computerSelection))
-
-    if (playerSelection == "rock" && computerSelection == "paper"){
-        computerScore ++;
+    playRound()
+    playRound()
+    playRound()
     
 
-    }else if (playerSelection == "scissors" && computerSelection == "paper"){
-        playerScore ++;
+    
 
-    }else if (playerSelection == "paper" && computerSelection == "rock"){
-        playerScore ++;
-
-    }else if (playerSelection == "scissors" && computerSelection == "rock"){
-        computerScore ++;
-    }else if (playerSelection  == "rock" && computerSelection == "scissors"){
-        playerScore ++;
-
-    }else if (playerSelection == "paper" && computerSelection == "scissors"){
-        computerScore ++ ; 
-    }
-    console.log(playerScore)
-    console.log(computerScore)
+    
 };
-
-// trouble? find out how to run 1 game at a time in console!
-
-
-
-
 
 game()
 
+
+
+// trouble? find out how to run 1 game at a time in console!
